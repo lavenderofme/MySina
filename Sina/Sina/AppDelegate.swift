e563be04f1b491df821b6065ce3a6478b0b9d57f
shasha <1979474783@qq.com>
2015-11-07 10:24:18 +0800

    04-自定义输出函数

--- Sina/Sina/AppDelegate.swift
+++ Sina/Sina/AppDelegate.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-@UIApplicationMain
+@UIApplicationMain // 相当于 main 函数, 是程序的入口
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // 全局有效的设置一般在程序一启动就设置,否则不知道何时有效,一般在 AppDelegate中设置
+        UITabBar.appearance().tintColor = UIColor.orangeColor()
         
         // 1.创建窗口
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
@@ -23,10 +25,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // 3.显示窗口
         window?.makeKeyAndVisible()
         
+        LQYLog(1)
+        LQYLog("wo")
+        LQYLog(11.09)
         
         return true
     }
 
+    /*
+    自定义LOG的目的:
+    在开发阶段自动显示LOG
+    在发布阶段自动屏蔽LOG
+
+    print(__FUNCTION__)  // 打印所在的方法
+    print(__LINE__)     // 打印所在的行
+    print(__FILE__)     // 打印所在文件的路径
+
+    方法名称[行数]: 输出内容
+    */
+    // T 相当于泛型,也就是说传什么就是什么
+    func LQYLog<T>(message: T, method: String =  __FUNCTION__, line: Int = __LINE__)
+    {
+        
+        #if DEBUG
+        print("\(method)[\(line)]:\(message)")
+        #endif
+    }
 
 }
 
--- Sina/Sina/Classes/Main/MainViewController.swift
+++ Sina/Sina/Classes/Main/MainViewController.swift
@@ -12,8 +12,21 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        UITabBar.appearance().tintColor = UIColor.orangeColor()
         
+        // 全局有效的设置一般在程序一启动就设置,否则不知道何时有效,一般在 AppDelegate中设置
+        // UITabBar.appearance().tintColor = UIColor.orangeColor()
+        
+        // 添加自控制器
+//        let home = HomeTableViewController()
+//        let nav = UINavigationController(rootViewController: home)
+//        addChildViewController(nav)
+//        
+//        home.tabBarItem.title = "首页"
+//        home.tabBarItem.image = UIImage(named: "tabbar_home")
+//        home.tabBarItem.selectedImage = UIImage(named: "tabbar_home_highlighted")
+//        // 注意: iOS7之前,设置 UITabBar 的 tintColor只有文字有效,而图片无效
+//        home.tabBarController?.tabBar.tintColor = UIColor.orangeColor()
+
         // 添加自控制器
         addChildControllers(HomeTableViewController(), image: "tabbar_home", title: "首页")
         addChildControllers(MessageViewController(), image: "tabbar_message_center", title: "消息")
