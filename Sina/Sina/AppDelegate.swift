87d67e52756f0556648200e308aeb230fbe73a72
shasha <1979474783@qq.com>
2015-11-09 13:34:55 +0800

    07-加号按钮

--- Sina/Sina/AppDelegate.swift
+++ Sina/Sina/AppDelegate.swift
@@ -17,14 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // 全局有效的设置一般在程序一启动就设置,否则不知道何时有效,一般在 AppDelegate中设置
         UITabBar.appearance().tintColor = UIColor.orangeColor()
-        
-//        // 1.创建窗口
-//        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-//        // 2.设置窗口根控制器
-//        window?.rootViewController = MainViewController()
-//        // 3.显示窗口
-//        window?.makeKeyAndVisible()
-        
+        /*
+        // 1.创建窗口
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        // 2.设置窗口根控制器
+        window?.rootViewController = MainViewController()
+        // 3.显示窗口
+        window?.makeKeyAndVisible()
+        */
         return true
     }
 
--- Sina/Sina/Classes/Main/ MainViewController.swift
+++ Sina/Sina/Classes/Main/ MainViewController.swift
@@ -12,73 +12,43 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 1.加载 JSON 数据
-        let path = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil)!
-        let data = NSData(contentsOfFile: path)!
-        
-        /* throws是XCode7最大的变化, 异常处理机制
-           1.do catch的作用: 一旦方法抛出异常, 那么就会执行catch后面{}中的内容, 如果没有抛出异常, 那么catch后面{}中的内容不执行
-                1.try : 正常处理异常, 一旦有异常就执行catch
-                2.try!: 强制处理异常(忽略异常),也就是说告诉系统一定不会发生异常, 如果真的发生了异常, 那么程序会崩溃
-                3.try?: 忽略异常, 告诉系统可能有异常也可能没有异常, 如果发生异常返回值就是nil, 如果没有发生异常, 会将返回值包装为一个可选类型的值
-           2.开发中推举使用try? 和 try , 不推荐使用try!
-        */
+ 
 
-        guard let objc = try? NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) else
-        {
-            LQYLog("没有 JSON 数据")
-            // 添加自控制器
-            addChildControllers("HomeViewController",image:  "tabbar_home", title: "首页")
-            addChildControllers("MessageViewController", image: "tabbar_message_center", title: "消息")
-            addChildControllers("DiscoverViewController", image: "tabbar_discover", title: "发现")
-            addChildControllers("ProfileViewController", image: "tabbar_profile", title: "我")
-            return
-        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
-        // 2.根据json文件返回的数据加载控制器
-        for dict in objc as! [[String : AnyObject]]
-        {
-            addChildControllers(dict["vcName"] as! String, image:dict["imageName"] as! String , title: dict["title"] as! String)
-        }
+        tabBar.addSubview(composeButton)
+        
+        let width = UIScreen.mainScreen().bounds.width / CGFloat(childViewControllers.count)
+        let height = composeButton.bounds.height
+        let y: CGFloat = 0
+        
+        composeButton.frame = CGRectMake(width * 2, y, width, height)
+    }
+    
+    // MARK: - 内部控制方法
+    func btnClick()
+    {
+        LQYLog("")
         
     }
     
-    /** 添加自控制器 */
-    func addChildControllers(childControllerName: String , image: String, title: String)
-    {
-        // 0.动态获取命名空间
-        guard let nsp =  NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as? String else
-        {
-            LQYLog("没有获取到命名空间")
-            return
-        }
+   
+    // MARK: - 懒加载
+    private lazy var composeButton: UIButton = {
         
-        // 1.通过字符串创建一个类
-        // 在 Swift 中, 如果向通过一个AnyClass来创建一个对象, 必须先明确这个类的类型
-        let cls: AnyClass? = NSClassFromString( nsp + "." + childControllerName)
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: UIControlState.Highlighted)
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: UIControlState.Normal)
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: UIControlState.Highlighted)
         
-        // 2.通过 class 创建一个类
-        // 在Swift中如果向通过一个AnyClass来创建一个对象, 必须先明确这个类的类型
-        guard let vcCls = cls as? UITableViewController.Type else
-        {
-            LQYLog("AnyClass没有值")
-            return
-        }
-        let childController = vcCls.init()
+        btn.addTarget(self, action: Selector("btnClick"), forControlEvents: UIControlEvents.TouchUpInside)
         
+        btn.sizeToFit()
+        return btn
         
-        // 3.1设置 tabBar 上的标题和图片
-        childController.title = title
-        childController.tabBarItem.image = UIImage(named:image)
-        childController.tabBarItem.selectedImage = UIImage(named:image + "_highlighted")
-        // 3.2包装一个导航控制器
-        let nav = UINavigationController(rootViewController: childController)
-        
-        // 3.3将导航控制器添加到 UITabBarController
-        addChildViewController(nav)
-        
-    }
-
-
+    }()
 }
